e66471a85abbfec7650167efb6d6e3b4387bcbdd
Kinga Zawarty <niu.ula.a@gmail.com>
2023-10-11 16:27:28 +0200

    some dynamic data

--- 01-starting-setup/src/components/CatComponent.js
+++ 01-starting-setup/src/components/CatComponent.js
@@ -1,11 +1,18 @@
 import './CatComponent.css';
 
 function Cat() {
+
+    const catName = "Leo";
+    const randomNum = Math.random();
+    const date = Date(2023, 10, 11);
+
+
     return (
         <div className="cat-item">
             <div className="cat-item__description">
-                <div className="cat-item__title">Leo</div>
-                <h2>leo to najslodszy kotek na swiecie</h2>
+                <div>{date}</div>
+                <div className="cat-item__title">{catName}</div>
+                <h2>leo to najslodszy kotek na swiecie  {randomNum}</h2>
             </div>
         </div>
     );
