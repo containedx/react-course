ef406470be3e3742f3fd50b7274f2af7814c697f
Kinga Zawarty <niu.ula.a@gmail.com>
2023-10-18 10:53:58 +0200

    use state

--- 01-starting-setup/src/components/CatComponent.js
+++ 01-starting-setup/src/components/CatComponent.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import './CatComponent.css';
 
 function Cat(props) {
@@ -6,6 +7,13 @@ function Cat(props) {
     const month = props.date.toLocaleString('en-US', { month: 'long' });
     const day = props.date.toLocaleString('en-US', { day: '2-digit' });
 
+    const [catName, setCatName] = useState(props.catName);
+
+    const clickHandler = () => {
+        setCatName("Leo Leo");
+        console.log("click");
+    };
+
     return (
         <div className="cat-item">
             <div className="cat-item__description">
@@ -13,9 +21,10 @@ function Cat(props) {
                     <div>{month}</div>
                     <div><h2>{day}</h2></div>
                     <div>{year}</div>
+                    <button onClick={clickHandler}>Click me</button>
                 </div>
 
-                <div className="cat-item__title">{props.catName}</div>
+                <div className="cat-item__title">{catName}</div>
                 <h2>lorem ipsum  hd icnd hudhs huh sin ament gin dan banana</h2>
             </div>
         </div>
