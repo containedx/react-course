52cc338406f756d8ba2f3286fe33cdb640910d10
Kinga Zawarty <niu.ula.a@gmail.com>
2024-06-23 17:44:30 +0200

    button input use state

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,12 +1,27 @@
+import { useState } from 'react';
 
 export default function Player({name, symbol}){
+
+    const[ isEditing, setIsEditing ] = useState(false);
+
+    function onClick(){
+        setIsEditing(!isEditing);
+    }
+
+    let playerName = <span className="player-name">{name}</span>;
+    let buttonName = "Edit";
+    if(isEditing) {
+        playerName = <input type="text" required></input>
+        buttonName = "Submit";
+    }
+
     return(
         <li>
             <span className="player">
-                <span className="player-name">{name}</span>
+                {playerName}
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={onClick}>{buttonName}</button>
         </li>
     );
 }
