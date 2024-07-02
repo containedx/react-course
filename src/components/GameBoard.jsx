7243ccb2cbaf2f7fb8aa4830e27ec79e542cd2df
Kinga Zawarty <niu.ula.a@gmail.com>
2024-07-02 08:40:16 +0200

    draw x on board

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 
 const initialGameBoard = [
     [null, null, null],
@@ -7,12 +8,22 @@ const initialGameBoard = [
 
 export default function GameBoard() {
 
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+    function handleSelectSquare(row, col){
+        setGameBoard((prevGameBoard) => {
+            const updatedBoard = [...prevGameBoard.map(innerArr => [...innerArr])];
+            updatedBoard[row][col] = 'X';
+            return updatedBoard;
+        });
+    }
+
     return(
         <ol id="game-board">
-            {initialGameBoard.map((row, rowIndex) => <li key={rowIndex}>
+            {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
                 <ol>
                     {row.map((playerSymbol, colIndex) => <li key={colIndex}>
-                        <button>{playerSymbol}</button>
+                        <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
                         </li>)}
                 </ol>
             </li>)}
