cc42a7b2c6b05cd871e84de46bbc83ecd09f3ee0
Kinga Zawarty <niu.ula.a@gmail.com>
2024-07-07 13:16:48 +0200

    restart page and game

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,8 +1,14 @@
 export default function GameOver({winner}) {
+
+function Restart()
+{
+    window.location.reload();
+}
+
     return <div id="game-over">
         <h2> GAME OVER! </h2>
         <h1>{winner}</h1>
 
-        <button>Restart</button>
+        <button onClick={Restart}>Restart</button>
     </div>
 }
